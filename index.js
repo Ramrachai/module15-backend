9ff695f3a090cf2ff0caa1bdd7a2932601ee0c04
ram-elife <ramrachai@elifetransfer.com>
2023-11-29 00:42:52 +0600

    fix: configured  cors and allowed all

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 app.get('/', (request, response) => {
   console.log(request);
