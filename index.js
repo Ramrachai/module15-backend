04cb218ceeb2c946aa2cc14fc7e845e0916f3685
ram-elife <ramrachai@elifetransfer.com>
2023-11-29 00:56:47 +0600

    cors update

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173/",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.get('/', (request, response) => {
   console.log(request);
@@ -29,3 +28,4 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
