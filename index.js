fe9bbbebf2483af95d8dd347c8f8676d4413eea5
Ramrachai Marma <47687976+Ramrachai@users.noreply.github.com>
2023-11-29 00:50:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:5173/",
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
